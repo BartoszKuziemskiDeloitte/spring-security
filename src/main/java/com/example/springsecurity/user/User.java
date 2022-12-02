c5d1ad1e40a7f56deea75851aef5368e1dd08f52
BartoszKuziemski <bartuspl99@gmail.com>
2022-12-03 00:30:54 +0100

    user entity added

---
+++ src/main/java/com/example/springsecurity/user/User.java
@@ -0,0 +1,41 @@
+package com.example.springsecurity.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "surname")
+    private String surname;
+
+}
